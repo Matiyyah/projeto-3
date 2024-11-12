1551b8cc7ed7e2f3986edafa6962545efc0c46d4
Matiyyah <matheuscoliveira06@gmail.com>
2024-11-12 00:49:12 -0300

    atualização sobre

--- static/scipt.js
+++ static/scipt.js
@@ -1,10 +1,10 @@
 /* abre e fecha menu lateral em modo mobile */
 /* deu merda rever depois */
 
-const menuMobile = document.querySelector(".menu-mobile");
-const body = document.querySelector("body");
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
 
-menuMobile.addEventListener("click", () => {
+menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
     ? menuMobile.classList.replace("bi-list, bi-x")
     : menuMobile.classList.replace("bi-x, bi-list");
